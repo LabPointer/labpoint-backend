0ec409a430215bd9325cbebf3bd1a63577a0455d
Nathan Miguel <nathan.cm77@gmail.com>
2026-05-08 11:49:08 -0300

    removed spring security

--- src/main/java/com/backend/labpoint/config/SecurityConfig.java
+++ src/main/java/com/backend/labpoint/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.backend.labpoint.config;
 
 import org.springframework.context.annotation.Bean;
@@ -20,3 +21,4 @@ public class SecurityConfig {
         return http.build();
     }
 }
+*/
